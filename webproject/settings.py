fbf94bcd04bd049949fbb287d1bfb02ff50e614b
{HangaMaster} <{adrianegeajimenez@gmail.com}>
2023-07-11 17:50:36 +0200

    Subida de web a Pythonanywhere

--- webproject/settings.py
+++ webproject/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@m$-1jb0564e$%8zt0sn-8p-r912^6(9$_o3wfn*@x9j%qzzz%'
+SECRET_KEY = open('.secret_key', 'r', encoding='utf-8').read()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'testapp',
 ]
 
 MIDDLEWARE = [
